0b6c06c350ec49f183c2b1387c202b768d5aa042
Indhumathy1998 <73114841+Indhumathy1998@users.noreply.github.com>
2020-11-08 15:18:49 +0530

    Update Sum_of_three_integer_adds_to_0.sh

--- Day7_Array_programs/Sum_of_three_integer_adds_to_0.sh
+++ Day7_Array_programs/Sum_of_three_integer_adds_to_0.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -x
 
-counter=0
-Number[((counter++))]="1"
-Number[((counter++))]="-2"
-Number[((counter++))]="1"
-echo ${Number[@]}
+echo -n "enter number of elements in the array: "
+read number
+echo "enter $number elements" number
+for(( i = 0; i < number; i++ ))
+do
+    read value
+    array[i]=$value
+done
+sum=0
+for(( i = 0; i < number - 2; i++ ))
+do
+    for(( j = i + 1; j < number - 1; j++ ))
+    do
+        for(( k = j + 1; k < number; k++ ))
+        do
+            if(( array[i] + array[j] + array[k] == sum))
+            then
+                echo "The sum of three integers is zero"
+            fi
+        done
+    done
+done
 
-sum=$(( ${Number[0]}+ ${Number[1]}+ ${Number[2]} ))
-echo "Sum of three integers: (${Number[0]})+(${Number[1]})+(${Number[2]})=" $sum
